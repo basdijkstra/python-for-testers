0545f4df49a6c6e4568e6736d219ffe22858d593
Bas Dijkstra <bas@ontestautomation.com>
2019-11-05 23:20:04 +0000

    Added examples for first two series of exercises

---
+++ examples/examples_01.py
@@ -0,0 +1,11 @@
+print("Your print statement goes here")
+
+my_string_variable = "my_variable_value"
+print("My string variable has a value of " + my_string_variable)
+
+my_int_variable = 42
+print("My int variable has a value of " + str(my_int_variable))
+
+my_user_input = input("Your user input goes here: ")
+print("The user input was " + my_user_input)
+

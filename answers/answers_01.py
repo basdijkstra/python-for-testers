54fc3079d1489d4e5de9919ae09592834b34e2c5
Bas Dijkstra <bas@ontestautomation.com>
2019-11-05 22:46:23 +0000

    Initial commit

---
+++ answers/answers_01.py
@@ -0,0 +1,22 @@
+# Exercise 1.1
+# Print "Hello world!" to the console
+print("Hello world!")
+
+# Exercise 1.2
+# Create a new variable my_name and assign it your first name
+# Print "My name is <my_name" to the console
+my_name = "Bas"
+print("My name is " + my_name)
+
+# Exercise 1.3
+# Create a new variable my_age and assign it your age
+# Print "I am <my_age> years old" to the console
+my_age = 39
+print("I am " + str(my_age) + " years old")
+
+# Exercise 1.4
+# Ask the user (nicely!) to input their city of residence
+# Store the answer in a new variable my_city and
+# use it to print "I live in <my_city>"
+my_city = input("Where do you live?")
+print("I live in " + my_city)
---
+++ exercises/exercises_01.py
@@ -0,0 +1,19 @@
+# Exercise 1.1
+# Print "Hello world!" to the console
+
+
+# Exercise 1.2
+# Create a new variable my_name and assign it your first name
+# Print "My name is <my_name" to the console
+
+
+# Exercise 1.3
+# Create a new variable my_age and assign it your age
+# Print "I am <my_age> years old" to the console
+
+
+# Exercise 1.4
+# Ask the user (nicely!) to input their city of residence
+# Store the answer in a new variable my_city and
+# use it to print "I live in <my_city>"
+

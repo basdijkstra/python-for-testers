04ebd62774bf70a592aa7cb9bdbbc3050332d849
Bas Dijkstra <bas@ontestautomation.com>
2019-11-13 13:49:24 +0100

    Updated series 2 exercises descriptions

--- api_tests/exercises/exercises_02.py
+++ api_tests/exercises/exercises_02.py
@@ -23,12 +23,15 @@ import csv
 # 1,Leanne Graham,Gwenborough
 # 4,Patricia Lebsack,South Elvis
 # 9,Glenna Reichert,Bartholomebury
-# Store this test data file in the exercises folder
+# Create a method write_csv() that creates this .csv file using the test_data_users
+# object from Exercise 2.1
 
 
 # Exercise 2.4
 # Create a method read_csv() that reads the file from 2.3 line by line
 # and creates and returns a test data object from the data in the .csv file
+# On the first line of the method, call the write_csv() method from Exercise 2.3
+# to ensure that it exists
 
 
 # Exercise 2.5

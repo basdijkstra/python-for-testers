b7719b9fab7e357d6839b0fa5a48b6fdbbc09da1
Bas Dijkstra <bas@ontestautomation.com>
2019-12-09 12:12:30 +0100

    Fixed some more helpers

--- ui_tests/exercises/helpers/selenium_helpers.py
+++ ui_tests/exercises/helpers/selenium_helpers.py
@@ -8,21 +8,21 @@ class SeleniumHelpers:
         self.browser = browser
 
     def send_keys(self, locator, text_to_type):
-        element = WebDriverWait(self.browser, 10).until(ec.element_to_be_clickable((locator[0], locator[1])))
+        element = WebDriverWait(self.browser, 10).until(ec.element_to_be_clickable(locator))
         element.send_keys(text_to_type)
 
     def click(self, locator):
-        element = WebDriverWait(self.browser, 10).until(ec.element_to_be_clickable((locator[0], locator[1])))
+        element = WebDriverWait(self.browser, 10).until(ec.element_to_be_clickable(locator))
         element.click()
 
     def select(self, locator, value_to_select):
-        element = Select(WebDriverWait(self.browser, 10).until(ec.element_to_be_clickable((locator[0], locator[1]))))
+        element = Select(WebDriverWait(self.browser, 10).until(ec.element_to_be_clickable(locator)))
         element.select_by_visible_text(value_to_select)
 
     def is_displayed(self, locator):
-        element = WebDriverWait(self.browser, 10).until(ec.visibility_of_element_located((locator[0], locator[1])))
+        element = WebDriverWait(self.browser, 10).until(ec.visibility_of_element_located(locator))
         return element.is_displayed()
 
     def get_element_text(self, locator):
-        element = WebDriverWait(self.browser, 10).until(ec.visibility_of_element_located((locator[0], locator[1])))
+        element = WebDriverWait(self.browser, 10).until(ec.visibility_of_element_located(locator))
         return element.text
